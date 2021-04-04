ef696f26371341c031c046efb3d5ccd49dbc8975
Igork777 <igorlopatchin27@gmail.com>
2021-04-04 19:49:15 +0200

    Initial log in, which doesn't work efficiently

--- app/src/main/java/com/example/faunaapp/view/fragments/LogInFragment.java
+++ app/src/main/java/com/example/faunaapp/view/fragments/LogInFragment.java
@@ -1,5 +1,8 @@
 package com.example.faunaapp.view.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,47 +13,78 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.faunaapp.R;
 import com.example.faunaapp.view_model.LogInFragmentViewModel;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Objects;
 
 public class LogInFragment extends Fragment {
 
+
     private Button logInButton;
     private TextInputLayout email, password;
     private LogInFragmentViewModel logInViewModel;
     private View logInView;
-    private String errorMessage;
+    private SharedPreferences localStorage;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        localStorage = getActivity().getSharedPreferences("data", Context.MODE_PRIVATE);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         logInView = inflater.inflate(R.layout.log_in_fragment, container, false);
-        initializeFragmentsValues(logInView);
-//        logInViewModel.getErrorMessage(email.getEditText().getText().toString(), password.getEditText().getText().toString()).observe(getViewLifecycleOwner(), s -> {
-//
-//        });
-      logInButton.setOnClickListener(view -> {
-          logInViewModel.logIn(Objects.requireNonNull(email.getEditText()).getText().toString(), Objects.requireNonNull(password.getEditText()).getText().toString());
+        initializeFragmentsValues();
+        logInButton.setOnClickListener(view -> {
+            logInViewModel.logIn(Objects.requireNonNull(email.getEditText()).getText().toString(), Objects.requireNonNull(password.getEditText()).getText().toString());
         });
         return logInView;
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
 
-    private void initializeFragmentsValues(View view) {
+    @Override
+    public void onResume() {
+        super.onResume();
+//        localStorage.getString("token", "no token");
+    }
+
+    private void initializeFragmentsValues() {
         logInViewModel = new ViewModelProvider(this).get(LogInFragmentViewModel.class);
-        logInButton = view.findViewById(R.id.log_in_button);
-        email = view.findViewById(R.id.emailInput);
-        password = view.findViewById(R.id.passwordInput);
+        logInButton = logInView.findViewById(R.id.log_in_button);
+        email = logInView.findViewById(R.id.emailInput);
+        password = logInView.findViewById(R.id.passwordInput);
+        setUpObserver();
+    }
+
+    private void setUpObserver() {
+        logInViewModel.getErrorMessage().observe(getViewLifecycleOwner(), errorMessage -> {
+            if (!errorMessage.equals("")) {
+             Snackbar snackbar = Snackbar.make(logInView, errorMessage, Snackbar.LENGTH_LONG).setAction("Retry", view -> {
+                });
+             snackbar.setTextColor(Color.RED);
+             snackbar.show();
+            }
+        });
+
+        logInViewModel.getToken().observe(getViewLifecycleOwner(), token-> {
+            System.out.println("Token is here: " + token);
+            SharedPreferences.Editor editor = localStorage.edit();
+            editor.putString("token", token);
+            editor.apply();
+
+        });
     }
 
 
--- app/src/main/java/com/example/faunaapp/view_model/LogInFragmentViewModel.java
+++ app/src/main/java/com/example/faunaapp/view_model/LogInFragmentViewModel.java
@@ -1,23 +1,52 @@
 package com.example.faunaapp.view_model;
 
+import android.os.AsyncTask;
+
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class LogInFragmentViewModel extends ViewModel {
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+import com.example.faunaapp.repository.Repository;
+import com.faunaapp.graphql.LogInMutation;
+
+import org.jetbrains.annotations.NotNull;
+
+public class LogInFragmentViewModel extends ViewModel
+{
     private MutableLiveData<String> errorMessage;
     private String errorConstructor;
+    private Repository repository;
+    private MutableLiveData<String> token;
 
     public LogInFragmentViewModel() {
+        repository = Repository.getInstance();
         errorMessage = new MutableLiveData<>();
+        token = new MediatorLiveData<>();
     }
 
     public LiveData<String> getErrorMessage(){
         return errorMessage;
     }
 
+    public LiveData<String> getToken() {
+        token.postValue(repository.getToken());
+        return token;
+    }
+
     public void logIn(String email, String password)
     {
+        if(checkIfEmailAndPasswordExists(email, password))
+        {
+            repository.logIn(email, password);
+        }
+    }
+
+    private boolean checkIfEmailAndPasswordExists(String email, String password) {
         errorConstructor = "";
         if(email.equals(""))
         {
@@ -27,6 +56,11 @@ public class LogInFragmentViewModel extends ViewModel {
         {
             errorConstructor += "Please, provide your password";
         }
+        if(errorConstructor.equals(""))
+        {
+            return true;
+        }
         errorMessage.postValue(errorConstructor);
+        return false;
     }
 }
